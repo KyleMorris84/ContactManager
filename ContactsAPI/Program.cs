e33aebb284f48849f82d774fd22550539a01b5c3
Kyle <kylemorris8484@gmail.com>
2025-05-09 19:34:02 +0100

    Allow CORS for dev

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -10,12 +10,22 @@ builder.Services.AddDbContext<ContactsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ContactsDb"));
 });
 builder.Services.AddScoped<IContactsRepository, ContactsRepository>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_myAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                          policy.AllowAnyHeader();
+                      });
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UseCors("_myAllowSpecificOrigins");
 
 DbInitialiser.Seed(app);
 app.Run();
